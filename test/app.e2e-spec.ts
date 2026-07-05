fa7af04cb6973181ed14df8dba0979a7b9d61ef1
aimanbanihani <aimanabnihani@gmail.com>
2026-07-05 16:49:32 +0300

    /health

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -18,9 +18,9 @@ describe('AppController (e2e)', () => {
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/health')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Mazoom API is running');
   });
 
   afterEach(async () => {
